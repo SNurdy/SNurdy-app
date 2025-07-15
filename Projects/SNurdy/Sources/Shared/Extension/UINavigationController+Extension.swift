d367df6997d37f03fd5818201d09be18b0f1ff3f
Yang Sijun <yang@sijun.dev>
2025-07-15 16:45:44 +0900

    feat: Implement Swipe back gesture

---
+++ Projects/SNurdy/Sources/Shared/Extension/UINavigationController+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController+Extension.swift
+//  Projects
+//
+//  Created by 양시준 on 7/15/25.
+//
+
+import SwiftUI
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
